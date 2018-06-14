2d67887f59c6fd6403731a565f16503645493834
Ritesh Singh <1124ritesh@gmail.com>
2018-06-14 16:07:55 +0530

    dashboard tasks panel'
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ theme.js
@@ -0,0 +1,10 @@
+// sidenav toggle
+$(document).ready(function () {
+    $("#menu").click(function () {
+        $("#sidenav").toggle();
+    });
+    $("#menu2").click(function () {
+        $("#sidenav").toggle();
+    });
+});
+
