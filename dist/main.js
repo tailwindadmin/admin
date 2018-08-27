712beec3ec4713b33c72824179760f77300f1c78
Ritesh Singh <1124ritesh@gmail.com>
2018-08-27 19:26:13 +0530

    sidebar toggle
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+var sidebar = document.getElementById('sidebar');
+
+function sidebarToggle() {
+    if (sidebar.style.display === "none") {
+        sidebar.style.display = "block";
+    } else {
+        sidebar.style.display = "none";
+    }
+}
