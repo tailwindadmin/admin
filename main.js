1c6427d99f0c9c260ac1893f8d5a7809724f1b0a
wassimbj <wassimbenjdida@gmail.com>
2019-11-24 11:32:34 +0100

    Finished the Modal popups

--- main.js
+++ main.js
@@ -17,3 +17,37 @@ function profileToggle() {
         profileDropdown.style.display = "none";
     }
 }
+
+
+/**
+ * ### Modals ###
+ */
+
+function toggleModal(action, elem_trigger)
+{
+    elem_trigger.addEventListener('click', function () {
+        if (action == 'add') {
+            let modal_id = this.dataset.modal;
+            document.getElementById(`${modal_id}`).classList.add('modal-is-open');
+        } else {
+            // Automaticlly get the opned modal ID
+            let modal_id = elem_trigger.closest('.modal-wrapper').getAttribute('id');
+            document.getElementById(`${modal_id}`).classList.remove('modal-is-open');
+        }
+    });
+}
+
+
+// Check if there is modals on the page
+if (document.querySelector('.modal-wrapper'))
+{
+    // Open the modal
+    document.querySelectorAll('.modal-trigger').forEach(btn => {
+        toggleModal('add', btn);
+    });
+    
+    // close the modal
+    document.querySelectorAll('.close-modal').forEach(btn => {
+        toggleModal('remove', btn);
+    });
+}
